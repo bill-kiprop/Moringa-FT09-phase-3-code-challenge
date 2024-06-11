0d50d38f5df6ae58a3b2bf3bf59dbe02fc2ae616
billkiprop <kipropbill3@gmail.com>
2024-06-11 15:09:21 +0300

    completed code challange

--- models/article.py
+++ models/article.py
@@ -1,10 +1,57 @@
-class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
-        self.title = title
-        self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
+from database.connection import get_db_connection
 
-    def __repr__(self):
-        return f'<Article {self.title}>'
+class Article:
+    def __init__(self, id, title, content, author, magazine):
+        
+        self._title = title
+        self._content = content
+        self._author = author
+        self._magazine = magazine
+        self._id = id
+
+        
+        conn = get_db_connection()
+        c = conn.cursor()
+        c.execute('''
+                    INSERT INTO articles (title, content, author_id, magazine_id) 
+                    VALUES (?, ?, ?, ?)
+                ''', 
+        (title, content, author.id, magazine.id))
+        conn.commit()
+        self._id = c.lastrowid
+        conn.close()
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Title attribute is read-only")
+
+    @property
+    def content(self):
+        return self._content
+
+    @content.setter
+    def content(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("Content must be a non-empty string")
+        self._content = value
+        conn = get_db_connection()
+        c = conn.cursor()
+        c.execute('UPDATE articles SET content = ? WHERE id = ?', (value, self._id))
+        conn.commit()
+        conn.close()
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
--- models/author.py
+++ models/author.py
@@ -1,7 +1,28 @@
-class Author:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+from database.connection import get_db_connection
 
-    def __repr__(self):
-        return f'<Author {self.name}>'
+class Author:
+    def __init__(self, id,name):
+        self._id = id
+        self._name = name
+
+      
+        conn = get_db_connection()
+        c = conn.cursor()
+        c.execute('INSERT INTO authors (name) VALUES (?)', (name,))
+        conn.commit()
+        self._id = c.lastrowid
+        conn.close()
+
+
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Name attribute is read-only")
